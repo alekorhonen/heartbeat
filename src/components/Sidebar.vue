e0130909f6312a043998231eecb575ce7a847381
Cryptofer <31718756+Cryptofer@users.noreply.github.com>
2020-05-05 15:38:55 +0300

    Modal component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="sidebar">
+      <ul>
+            <li class="brand-name">
+                <img src="../assets/HeartBeat.png" />
+            </li>
+            <li class="title">Navigation</li>
+            <li><router-link to="/"><font-awesome-icon icon="home" class="icon" />&nbsp; Home</router-link></li>
+            <li><router-link to="/songs"><font-awesome-icon icon="music" class="icon" />&nbsp; Songs</router-link></li>
+            <li><router-link to="/playlists"><font-awesome-icon icon="book" class="icon" />&nbsp; Library</router-link></li>
+            <li class="title">Playlists</li>
+            <li><router-link to="/#">House</router-link></li>
+            <li><button @click="open"><font-awesome-icon icon='plus' style='margin-right: 10px'/>Add a new Playlist</button></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Modal from '@/components/modals/new-playlist'
+export default {
+    methods: {
+        open() {
+            this.$modal.show(Modal, {
+                text: 'This text is passed as a property'
+            })
+        }
+    }
+}
+</script>
---
+++ src/components/modals/new-playlist.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <div class="modal">
+        123123123
+    </div>
+</template>
+
+<style>
+.vm--modal {
+    background-color: black !important;
+    box-shadow: 0 20px 60px -2px rgba(0, 0, 0, 0.4) !important;
+}
+</style>
