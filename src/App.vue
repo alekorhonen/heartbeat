9f64f59c59c60bba622038f6b62e3ce5192b942e
Cryptofer <31718756+Cryptofer@users.noreply.github.com>
2020-05-05 14:19:46 +0300

    Remove firebase & google api

--- src/App.vue
+++ src/App.vue
@@ -23,10 +23,6 @@
     </div>
     <div class="container">
       <div class="top-bar">
-        <div class="user-nav right">
-          <button class="login" @click="login" v-if="!LoggedIn">Login</button>
-          <button class="login" @click="logout" v-if="LoggedIn">Logout</button>
-        </div>
         <form class="search-form" @submit.prevent="search">
           <a 
             href="#" 
@@ -57,8 +53,6 @@
 </template>
 
 <script>
-import firebase from 'firebase'
-import axios from 'axios'
 import Player from '@/components/Player'
 
 export default {
@@ -74,63 +68,9 @@ export default {
       displayName: null
     }
   },
-  created() {
-    // if(firebase.auth().currentUser) {
-    //   this.LoggedIn = true
-    //   this.displayName = firebase.auth().currentUser.displayName
-    //   this.userPhoto = firebase.auth().currentUser.photoURL
-    // }
-  },
   methods: {
     clearSearch() {
       this.searchQuery = ''
-    },
-    login() {
-      // var provider = new firebase.auth.GoogleAuthProvider();
-      // provider.addScope('https://www.googleapis.com/auth/youtube.readonly')
-      // firebase.auth().useDeviceLanguage();
-      // firebase.auth().signInWithRedirect(provider);
-      // firebase.auth().getRedirectResult().then((result) => {
-      //   console.log(result)
-      //   if (result.credential) {
-      //     // This gives you a Google Access Token. You can use it to access the Google API.
-      //     var token = result.credential.accessToken;
-      //   }
-      // }).catch((error) => {
-
-      // });
-
-      this.$getGapiClient()
-      .then(gapi => {
-        this.getAuthInstance().signIn();
-      })
-    },
-    logout() {
-      // let that = this
-      // firebase.auth().signOut().then(function() {
-      //   that.LoggedIn = false
-      // }).catch(function(error) {
-      //   console.log(error.message)
-      // });
-
-      
-    },
-    search() {
-      this.$getGapiClient()
-      .then(gapi => {
-        var request = gapi.client.youtube.search.list({
-          part: '',
-          type: 'video',
-          q: encodeURIComponent(this.searchQuery).replace(/%20/g, '+'),
-          maxResults: 3,
-          order: 'viewCount'
-        })
-
-        //Execute request
-        request.execute((response) => {
-          console.log(response)
-        })
-      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
-import firebase from 'firebase';
-import VueGAPI from "vue-gapi";
 import App from './App.vue'
 import router from './router'
 import { store } from './store/store'
@@ -10,38 +8,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add({faPlus, faHome, faBook, faMusic, faPlayCircle, faPauseCircle, faBackward, faForward, faVolumeUp, faVolumeMute, faChevronLeft, faChevronRight, faChevronDown })
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.use(firebase) 
 
 Vue.config.productionTip = false
 
-let app = '';
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBNY5k5zx0xz7WNONNkCymNeM72tCqwh84",
-  authDomain: "client-dd43d.firebaseapp.com",
-  databaseURL: "https://client-dd43d.firebaseio.com",
-  projectId: "client-dd43d",
-  storageBucket: "client-dd43d.appspot.com",
-  messagingSenderId: "267952330576"
-};
-firebase.initializeApp(config);
-
-//Initialize Gapi
-const apiConfig = {
-  apiKey: "AIzaSyAXMFSWuGx4Y8OF1B6IUrUBrpxvHUMnEqA",
-  clientId: "267952330576-tr90lrtt5e83mectb0chq2qb7j26o993.apps.googleusercontent.com",
-  scope: "https://www.googleapis.com/auth/youtube.readonly"
-  // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
-};
-
-Vue.use(VueGAPI, apiConfig)
-
-firebase.auth().onAuthStateChanged(() => {
-  if (!app) {
-    app = new Vue({
-      router,
-      store,
-      render: h => h(App)
-    }).$mount('#app')
-  }
-});
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
